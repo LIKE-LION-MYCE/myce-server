22950d816ff8559ff04265fbf1d334258e4ef222
choeihwa <chldlghk@gmail.com>
2025-08-05 14:22:42 +0900

    feat: add signup api and basic security setting

---
+++ src/main/java/com/myce/auth/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.myce.auth.controller;
+
+import com.myce.auth.dto.SignupRequest;
+import com.myce.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody SignupRequest signupRequest) {
+        authService.signup(signupRequest);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/myce/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.myce.member.repository;
+
+import com.myce.member.entity.Member;
+import com.myce.member.entity.type.ProviderType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
