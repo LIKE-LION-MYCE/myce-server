b8b33d94d3fc96be0f6efc80e52aab7de3f7a3ef
ufo9430 <ufo9430@naver.com>
2025-08-06 00:34:56 +0900

    refactor: change method name

--- src/main/java/com/myce/advertisement/service/PlatformAdminAdvertisementService.java
+++ src/main/java/com/myce/advertisement/service/PlatformAdminAdvertisementService.java
@@ -4,8 +4,8 @@ import com.myce.advertisement.dto.SimpleApplyAdvertisement;
 import com.myce.common.dto.PageResponse;
 
 public interface PlatformAdminAdvertisementService {
-    PageResponse<SimpleApplyAdvertisement> getList(int page, int pageSize, boolean latestFirst);
+    PageResponse<SimpleApplyAdvertisement> getAllApplyList(int page, int pageSize, boolean latestFirst);
 
-    PageResponse<SimpleApplyAdvertisement> getFiltersByKeyword(String keyword, String status,
-                                                               int page, int pageSize, boolean latestFirst);
+    PageResponse<SimpleApplyAdvertisement> getFilteredApplyListByKeyword(String keyword, String status,
+                                                                         int page, int pageSize, boolean latestFirst);
 }
