2e5972faa9182107691fea7c0e6d4f8142fbe869
ufo9430 <ufo9430@naver.com>
2025-08-05 21:40:36 +0900

    refactor: apply pr feedback

--- src/main/java/com/myce/advertisement/repository/AdvertisementRepository.java
+++ src/main/java/com/myce/advertisement/repository/AdvertisementRepository.java
@@ -5,7 +5,6 @@ import com.myce.advertisement.entity.type.AdvertisementStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
