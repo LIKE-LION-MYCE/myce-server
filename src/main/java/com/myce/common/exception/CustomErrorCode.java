c22870f277a177e1fea2205725568b3f72e3ba21
ufo9430 <ufo9430@naver.com>
2025-08-05 19:57:11 +0900

    fix: fix typo

--- src/main/java/com/myce/common/exception/CustomErrorCode.java
+++ src/main/java/com/myce/common/exception/CustomErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CustomErrorCode {
     // 회원 M
-    MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M001", "회원정보가 존재하지 않습니다.");
+    MEMBER_NOT_EXIST(HttpStatus.NOT_FOUND, "M001", "회원정보가 존재하지 않습니다."),
 
     // 엑스포 E
 
