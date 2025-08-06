6dc9a4344779a0b903e135e4505954adfd43d1f5
Jihyun <qlfus010@naver.com>
2025-08-07 02:10:21 +0900

    feat : 현재 로그인한 사용자 기준 활성화된 박람회 리스트

--- src/main/java/com/myce/expo/controller/ExpoController.java
+++ src/main/java/com/myce/expo/controller/ExpoController.java
@@ -8,16 +8,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/expos")
 @RequiredArgsConstructor
 public class ExpoController {
   private final ExpoService exposervice;
+
+  // 관리하고 있는 박람회 리스트
+  @GetMapping("/my")
+  public ResponseEntity<List<Long>> getMyExpos(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+    Long memberId = customUserDetails.getMemberId();
+    return ResponseEntity.ok(exposervice.getMyExpos(memberId));
+  }
 
   // 박람회 등록
   @PostMapping
--- src/main/java/com/myce/expo/service/ExpoService.java
+++ src/main/java/com/myce/expo/service/ExpoService.java
@@ -2,7 +2,10 @@ package com.myce.expo.service;
 
 import com.myce.expo.dto.ExpoRegistrationRequest;
 
+import java.util.List;
+
 public interface ExpoService {
   void saveExpo(Long memberId, ExpoRegistrationRequest request);
+  List<Long> getMyExpos(Long memberId);
 }
 
