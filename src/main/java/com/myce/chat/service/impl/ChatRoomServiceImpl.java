e8ed3ab64e327c776e8b4a2644f3e5c29e025ad5
PG1Hub <parkpark827@naver.com>
2025-08-07 00:20:37 +0900

    fix:mapper file replace

--- src/main/java/com/myce/chat/service/impl/ChatRoomServiceImpl.java
+++ src/main/java/com/myce/chat/service/impl/ChatRoomServiceImpl.java
@@ -15,8 +15,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.myce.chat.mapper.ChatRoomMapper;
-
+import com.myce.chat.service.mapper.ChatRoomMapper;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
