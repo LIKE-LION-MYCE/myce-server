41b422aad9d60f3e0a1b064921e6f9cd9086f408
Jihyun <qlfus010@naver.com>
2025-08-10 02:04:00 +0900

    fix : merge 코드 충돌 수정

--- src/main/java/com/myce/expo/repository/AdminCodeRepository.java
+++ src/main/java/com/myce/expo/repository/AdminCodeRepository.java
@@ -4,6 +4,7 @@ import com.myce.expo.entity.AdminCode;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AdminCodeRepository extends JpaRepository<AdminCode, Long> {
