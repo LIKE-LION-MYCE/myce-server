e37eacb52a9365f85bc8a0608942025704fe4805
Jihyun <qlfus010@naver.com>
2025-08-11 03:54:24 +0900

    chore : 불필요한 import 제거

--- src/main/java/com/myce/MyceApplication.java
+++ src/main/java/com/myce/MyceApplication.java
@@ -2,10 +2,8 @@ package com.myce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableScheduling
