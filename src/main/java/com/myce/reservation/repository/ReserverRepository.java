cfc753f5bcd5c1dfa66a2e479fb975e20d997200
Jihyun <qlfus010@naver.com>
2025-08-11 04:08:17 +0900

    refactor : QR 상태값 분기 추가

--- src/main/java/com/myce/reservation/repository/ReserverRepository.java
+++ src/main/java/com/myce/reservation/repository/ReserverRepository.java
@@ -24,51 +24,46 @@ public interface ReserverRepository extends JpaRepository<Reserver, Long> {
     List<Reserver> findReserversByExpo(@Param("expoId") Long expoId);
 
     @Query("""
-            SELECT NEW com.myce.reservation.dto.ExpoAdminReservationResponse(
-              rv.id,
-              r.reservationCode,
-              rv.name,
-              CASE
-                WHEN rv.gender = com.myce.member.entity.type.Gender.FEMALE THEN '여'
-                WHEN rv.gender = com.myce.member.entity.type.Gender.MALE THEN '남'
-                ELSE '-'
-              END,
-              rv.phone,
-              rv.email,
-              t.name,
-              qc.usedAt,
-              CASE
-                WHEN qc.status = com.myce.qrcode.entity.code.QrCodeStatus.USED THEN '입장 완료'
-                WHEN qc.id IS NULL
-                     OR qc.status IN (
-                       com.myce.qrcode.entity.code.QrCodeStatus.ACTIVE,
-                       com.myce.qrcode.entity.code.QrCodeStatus.EXPIRED
-                     ) THEN '입장 전'
-                ELSE '입장 전'
-              END
+        SELECT NEW com.myce.reservation.dto.ExpoAdminReservationResponse(
+          rv.id,
+          r.reservationCode,
+          rv.name,
+          CASE
+            WHEN rv.gender = com.myce.member.entity.type.Gender.FEMALE THEN '여'
+            WHEN rv.gender = com.myce.member.entity.type.Gender.MALE THEN '남'
+            ELSE '-'
+          END,
+          rv.phone,
+          rv.email,
+          t.name,
+          qc.usedAt,
+          CASE
+            WHEN qc.status = com.myce.qrcode.entity.code.QrCodeStatus.USED THEN '입장 완료'
+            WHEN qc.status = com.myce.qrcode.entity.code.QrCodeStatus.EXPIRED THEN '티켓 만료'
+            WHEN qc.id IS NULL OR qc.status = com.myce.qrcode.entity.code.QrCodeStatus.ACTIVE THEN '입장 전'
+            ELSE '입장 전'
+          END
+        )
+        FROM Reserver rv
+        JOIN rv.reservation r
+        JOIN r.ticket t
+        LEFT JOIN com.myce.qrcode.entity.QrCode qc ON qc.reserver = rv
+        WHERE r.expo.id = :expoId
+          AND r.status = com.myce.reservation.entity.code.ReservationStatus.CONFIRMED
+          AND (:name IS NULL OR LOWER(rv.name) LIKE LOWER(CONCAT('%', :name, '%')))
+          AND (:phone IS NULL OR rv.phone LIKE CONCAT('%', :phone, '%'))
+          AND (:reservationCode IS NULL OR r.reservationCode LIKE CONCAT('%', :reservationCode, '%'))
+          AND (:ticketName IS NULL OR t.name = :ticketName)
+          AND (
+            :entranceStatus IS NULL OR
+            (
+              (:entranceStatus = '입장 완료' AND qc.status = com.myce.qrcode.entity.code.QrCodeStatus.USED)
+              OR (:entranceStatus = '입장 전' AND (qc.id IS NULL
+                  OR qc.status = com.myce.qrcode.entity.code.QrCodeStatus.ACTIVE))
+              OR (:entranceStatus = '티켓 만료' AND qc.status = com.myce.qrcode.entity.code.QrCodeStatus.EXPIRED)
             )
-            FROM Reserver rv
-            JOIN rv.reservation r
-            JOIN r.ticket t
-            LEFT JOIN com.myce.qrcode.entity.QrCode qc ON qc.reserver = rv
-            WHERE r.expo.id = :expoId
-              AND r.status = com.myce.reservation.entity.code.ReservationStatus.CONFIRMED
-              AND (:name IS NULL OR LOWER(rv.name) LIKE LOWER(CONCAT('%', :name, '%')))
-              AND (:phone IS NULL OR rv.phone LIKE CONCAT('%', :phone, '%'))
-              AND (:reservationCode IS NULL OR r.reservationCode LIKE CONCAT('%',:reservationCode,'%'))
-              AND (:ticketName IS NULL OR t.name = :ticketName)
-              AND (
-                :entranceStatus IS NULL OR
-                (
-                  (:entranceStatus = '입장 완료' AND qc.status = com.myce.qrcode.entity.code.QrCodeStatus.USED)
-                  OR (:entranceStatus = '입장 전' AND (qc.id IS NULL
-                      OR qc.status IN (
-                        com.myce.qrcode.entity.code.QrCodeStatus.ACTIVE,
-                        com.myce.qrcode.entity.code.QrCodeStatus.EXPIRED
-                      )))
-                )
-              )
-            """)
+          )
+    """)
     Page<ExpoAdminReservationResponse> findAllResponsesByExpoIdAndStatus(
             @Param("expoId") Long expoId,
             @Param("entranceStatus") String entranceStatus,
