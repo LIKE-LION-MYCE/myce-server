3d81d9d5869cfe60981cf538b43d205a9ea69c95
Jihyun <qlfus010@naver.com>
2025-08-15 20:46:27 +0900

    feat : QR 재발급 구현

--- src/main/java/com/myce/qrcode/service/impl/ExpoAdminQrServiceImpl.java
+++ src/main/java/com/myce/qrcode/service/impl/ExpoAdminQrServiceImpl.java
@@ -5,20 +5,27 @@ import com.myce.common.exception.CustomErrorCode;
 import com.myce.common.exception.CustomException;
 import com.myce.expo.repository.AdminPermissionRepository;
 import com.myce.expo.repository.ExpoRepository;
+import com.myce.qrcode.dto.ExpoAdminQrReissueRequest;
 import com.myce.qrcode.entity.QrCode;
 import com.myce.qrcode.entity.code.QrCodeStatus;
 import com.myce.qrcode.repository.QrCodeRepository;
-import com.myce.qrcode.service.ExpoAdminManualCheckInService;
+import com.myce.qrcode.service.ExpoAdminQrService;
 import com.myce.qrcode.service.QrCodeService;
 import com.myce.reservation.dto.ExpoAdminReservationResponse;
+import com.myce.reservation.entity.Reserver;
 import com.myce.reservation.repository.ReserverRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-public class ExpoAdminManualCheckInServiceImpl implements ExpoAdminManualCheckInService {
+public class ExpoAdminQrServiceImpl implements ExpoAdminQrService {
 
     private final ExpoRepository expoRepository;
     private final AdminPermissionRepository adminPermissionRepository;
@@ -49,6 +56,45 @@ public class ExpoAdminManualCheckInServiceImpl implements ExpoAdminManualCheckIn
             newQrCode.markAsUsed();
         }
         return reserverRepository.findOneResponsesByReserverId(reserverId,expoId);
+    }
+
+    @Override
+    public List<ExpoAdminReservationResponse> reissueReserverQrCode(Long expoId,
+                                                              Long memberId,
+                                                              LoginType loginType,
+                                                              ExpoAdminQrReissueRequest dto,
+                                                              String entranceStatus,
+                                                              String name,
+                                                              String phone,
+                                                              String reservationCode,
+                                                              String ticketName) {
+
+        validateMyAccess(expoId, memberId, loginType);
+
+        if ("입장 만료".equals(entranceStatus) || "티켓 만료".equals(entranceStatus) || "발급 대기".equals(entranceStatus)) {
+            throw new CustomException(CustomErrorCode.QR_INVALID_STATUS);
+        }
+
+        List<Long> reserverIds = dto.isSelectAllMatching()
+                ? reserverRepository.findReserversByFilter(expoId,entranceStatus,name,phone,reservationCode,ticketName)
+                .stream().map(Reserver::getId).toList()
+                : dto.getReserverIds();
+
+        for (Long reserverId : reserverIds){
+            QrCode existingQrCode = qrCodeRepository.findByReserverId(reserverId)
+                            .orElseThrow(() -> new CustomException(CustomErrorCode.QR_NOT_FOUND));
+
+            switch (existingQrCode.getStatus()) {
+                case USED, EXPIRED -> {
+                    throw new CustomException(CustomErrorCode.QR_INVALID_STATUS);
+                }
+                case ACTIVE, APPROVED -> {
+                    qrCodeService.reissueQr(reserverId, memberId, loginType);
+                }
+            }
+        }
+
+        return reserverRepository.findResponsesByReserverIds(reserverIds,expoId);
     }
 
     private void validateMyAccess(Long expoId, Long memberId, LoginType loginType) {
