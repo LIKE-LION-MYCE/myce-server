1c31eb7b2b76e88aee0b9200f068889eab647de9
choeihwa <chldlghk@gmail.com>
2025-08-17 04:12:33 +0900

    feat: modify email send structure

--- src/main/java/com/myce/system/service/message/impl/GenerateMessageServiceImpl.java
+++ src/main/java/com/myce/system/service/message/impl/GenerateMessageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.myce.system.service.message.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myce.common.exception.CustomErrorCode;
 import com.myce.common.exception.CustomException;
 import com.myce.system.dto.message.MessageTemplate;
@@ -8,14 +10,29 @@ import com.myce.system.entity.type.ChannelType;
 import com.myce.system.entity.type.MessageTemplateCode;
 import com.myce.system.repository.MessageTemplateSettingRepository;
 import com.myce.system.service.message.GenerateMessageService;
+import jakarta.annotation.PostConstruct;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
 public class GenerateMessageServiceImpl implements GenerateMessageService {
 
     private final MessageTemplateSettingRepository messageTemplateSettingRepository;
+    private final SpringTemplateEngine templateEngine;
+    private ObjectMapper objectMapper;
+
+    @PostConstruct
+    public void init() {
+        objectMapper = new ObjectMapper();
+    }
 
     @Override
     public MessageTemplate getMessageForVerification
@@ -24,12 +41,12 @@ public class GenerateMessageServiceImpl implements GenerateMessageService {
                 .findByCodeAndChannelType(MessageTemplateCode.EMAIL_VERIFICATION, ChannelType.EMAIL)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.NOT_EXIST_MESSAGE_TEMPLATE));
 
-        String content = messageTemplate.getContent();
-        content = content.replace("{{CODE}}", code);
-        content = content.replace("{{VERIFICATION_NAME}}", verificationName);
-        content = content.replace("{{LIMIT_TIME}}", limitTime);
+        Context context = new Context();
+        context.setVariable("code", code);
+        context.setVariable("limitTime", limitTime);
 
-        return new MessageTemplate(messageTemplate.getSubject(), content);
+        String message = getFullMessage(messageTemplate, context);
+        return new MessageTemplate(messageTemplate.getSubject(), message);
     }
 
     @Override
@@ -44,4 +61,22 @@ public class GenerateMessageServiceImpl implements GenerateMessageService {
         return new MessageTemplate(messageTemplate.getSubject(), content);
     }
 
+    private String getFullMessage(MessageTemplateSetting messageTemplate, Context context) {
+        Map<String, String> templateData = parseJsonContent(messageTemplate.getContent());
+
+        for (Map.Entry<String, String> entry : templateData.entrySet()) {
+            context.setVariable(entry.getKey(), entry.getValue());
+        }
+
+        return templateEngine.process("mail/mail-code",context);
+    }
+
+
+    private Map<String, String> parseJsonContent(String jsonContent) {
+        try {
+            return objectMapper.readValue(jsonContent, Map.class);
+        } catch (JsonProcessingException je) {
+            throw new IllegalArgumentException("JSON 파싱 실패: " + je.getMessage(), je);
+        }
+    }
 }
