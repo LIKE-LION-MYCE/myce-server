8ddc6d625e4842f3b77cc77e1b7d60df09452ba6
Jihyun <qlfus010@naver.com>
2025-08-17 04:11:13 +0900

    refactor : 페이지별 권한 검증 로직으로 리팩토링

--- src/main/java/com/myce/common/permission/ExpoAdminAccessValidate.java
+++ src/main/java/com/myce/common/permission/ExpoAdminAccessValidate.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /*
-    박람회 관리자의 api 요청에 대한 조회/편집 권한 검증을 위한 메소드입니다.
+    박람회 관리자의 api 요청에 대한 페이지별 조회/편집 권한 검증 메소드입니다.
  */
 @Component
 @RequiredArgsConstructor
@@ -18,27 +18,54 @@ public class ExpoAdminAccessValidate {
 
     private final ExpoRepository expoRepository;
     private final AdminPermissionRepository adminPermissionRepository;
-    
-    //GET 요청에서 사용 : 조회 권한 검증
+
+    //GET 메서드에서 사용 : 조회 권한 검증
     public void ensureViewable(Long expoId, Long memberId, LoginType loginType, ExpoAdminPermission permission) {
 
         //기본 유효성 검사
-        if (memberId == null || loginType == null) {
-            throw new CustomException(CustomErrorCode.MEMBER_NOT_EXIST);
-        }
-        if (permission == null) {
-            throw new CustomException(CustomErrorCode.INVALID_EXPO_ADMIN_PERMISSION_TYPE);
-        }
+        basicValidate(memberId, loginType, permission);
 
         //해당 엑스포의 상태가 조회 가능한 상태인지 확인
         ExpoStatus status = expoRepository.findStatusById(expoId)
                 .orElseThrow(() -> new CustomException(CustomErrorCode.EXPO_NOT_EXIST));
 
         if(!ExpoStatus.ADMIN_VIEWABLE_STATUSES.contains(status)) {
-            throw new CustomException(CustomErrorCode.EXPO_ACCESS_DENIED);
+            throw new CustomException(CustomErrorCode.EXPO_ACCESS_DENIED); //조회 가능한 상태가 아니라면 예외 반환(*)
         }
         
-        //박람회 관리자가 해당 엑스포에 대한 권한이 있는지 확인
+        //박람회 관리자가 해당 엑스포 페이지에 대한 권한이 있는지 확인
+        expoAdminValidate(expoId, memberId, loginType, permission);
+    }
+    
+    //PUT, POST, DELETE 등의 메소드에서 사용 : 편집 권한 검증
+    public void ensureEditable(Long expoId, Long memberId, LoginType loginType, ExpoAdminPermission permission) {
+        
+        //기본 유효성 검사
+        basicValidate(memberId, loginType, permission);
+
+        //해당 엑스포가 편집 가능한 상태인지 확인
+        ExpoStatus status = expoRepository.findStatusById(expoId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.EXPO_ACCESS_DENIED));
+
+        if(!ExpoStatus.ADMIN_EDITABLE_STATUSES.contains(status)) {
+            throw new CustomException(CustomErrorCode.EXPO_EDIT_DENIED); //편집이 가능한 상태가 아니라면 예외 반환(*)
+        }
+        
+        //박람회 관리자가 해당 엑스포 페이지에 대한 권한이 있는지 확인
+        expoAdminValidate(expoId, memberId, loginType, permission);
+    }
+
+    private void basicValidate(Long memberId, LoginType loginType, ExpoAdminPermission permission) {
+        if (memberId == null || loginType == null) {
+            throw new CustomException(CustomErrorCode.MEMBER_NOT_EXIST);
+        }
+
+        if (permission == null) {
+            throw new CustomException(CustomErrorCode.INVALID_EXPO_ADMIN_PERMISSION_TYPE);
+        }
+    }
+
+    private void expoAdminValidate(Long expoId, Long memberId, LoginType loginType, ExpoAdminPermission permission) {
         switch (loginType){
             case MEMBER -> {
                 if(!expoRepository.existsByIdAndMemberId(expoId, memberId)){
@@ -56,46 +83,6 @@ public class ExpoAdminAccessValidate {
                             -> adminPermissionRepository.existsByAdminCodeIdAndAdminCodeExpoIdAndIsEmailLogViewTrue(memberId,expoId);
                     case INQUIRY_VIEW
                             -> adminPermissionRepository.existsByAdminCodeIdAndAdminCodeExpoIdAndIsInquiryViewTrue(memberId,expoId);
-                    default -> throw new CustomException(CustomErrorCode.INVALID_EXPO_ADMIN_PERMISSION_TYPE);
-                };
-
-                if (!allowed) {
-                    throw new CustomException(CustomErrorCode.EXPO_ADMIN_PERMISSION_DENIED);
-                }
-            }
-            default -> throw new CustomException(CustomErrorCode.INVALID_LOGIN_TYPE);
-        }
-    }
-    
-    //POST, PUT, DELETE 등의 요청에서 사용 : 편집 권한 검증
-    public void ensureEditable(Long expoId, Long memberId, LoginType loginType, ExpoAdminPermission permission) {
-        
-        //기본 유효성 검사
-        if (memberId == null || loginType == null) {
-            throw new CustomException(CustomErrorCode.MEMBER_NOT_EXIST);
-        }
-        if (permission == null) {
-            throw new CustomException(CustomErrorCode.INVALID_EXPO_ADMIN_PERMISSION_TYPE);
-        }
-
-        //해당 엑스포가 편집 가능한 상태인지 확인
-        ExpoStatus status = expoRepository.findStatusById(expoId)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.EXPO_ACCESS_DENIED));
-
-        if(!ExpoStatus.ADMIN_EDITABLE_STATUSES.contains(status)) {
-            throw new CustomException(CustomErrorCode.EXPO_EDIT_DENIED);
-        }
-        
-        //박람회 관리자가 해당 엑스포에 대한 편집 권한이 있는지 확인
-        switch (loginType) {
-            case MEMBER -> {
-                if (!expoRepository.existsByIdAndMemberId(expoId, memberId)) {
-                    throw new CustomException(CustomErrorCode.EXPO_ACCESS_DENIED);
-                }
-            }
-
-            case ADMIN_CODE -> {
-                boolean allowed = switch(permission){
                     case EXPO_DETAIL_UPDATE
                             -> adminPermissionRepository.existsByAdminCodeIdAndAdminCodeExpoIdAndIsExpoDetailUpdateTrue(memberId,expoId);
                     case BOOTH_INFO_UPDATE
@@ -111,6 +98,7 @@ public class ExpoAdminAccessValidate {
                     throw new CustomException(CustomErrorCode.EXPO_ADMIN_PERMISSION_DENIED);
                 }
             }
+
             default -> throw new CustomException(CustomErrorCode.INVALID_LOGIN_TYPE);
         }
     }
--- src/main/java/com/myce/common/permission/ExpoAdminPermission.java
+++ src/main/java/com/myce/common/permission/ExpoAdminPermission.java
@@ -4,13 +4,17 @@ import com.myce.common.exception.CustomErrorCode;
 import com.myce.common.exception.CustomException;
 
 public enum ExpoAdminPermission {
+    /*
+        DB 필드명을 기준으로 작성했습니다.
+        UPDATE, VIEW 와 상관없이 탭별, 즉 페이지 기준 권한으로 정의합니다.
+     */
     EXPO_DETAIL_UPDATE,
     BOOTH_INFO_UPDATE,
     SCHEDULE_UPDATE,
+    OPERATIONS_CONFIG_UPDATE,
     RESERVER_LIST_VIEW,
     PAYMENT_VIEW,
     EMAIL_LOG_VIEW,
-    OPERATIONS_CONFIG_UPDATE,
     INQUIRY_VIEW;
 
     public static ExpoAdminPermission fromValue(String value) {
