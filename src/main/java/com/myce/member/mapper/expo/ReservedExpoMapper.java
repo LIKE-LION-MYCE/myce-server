880fbd74b2c82ab8aa5b1e30ad93facb0a61d83f
구본엽 <piusku2@gmail.com>
2025-08-16 16:51:10 +0900

    feat : 결제 완료 시 ADVERTISEMENT 테이블 -> PENDING_PUBLSIEHD ADVERTISEMENT_PAYMENT -> 전환 박람회 신청 광고 신청 화면 요금제 안내 박람회 취소됐을떄 CANCELLED일떄  상태값 뱃지 광고 결제 완료 시  메인페이지 - 박람회 상세 - 상세 정보 UI 수정 필요
    꼬마관리자 로그인 대시보드 리다이렉트 기능 추가
    티켓 정보 -> 판매 기간추가
    티켓 구매 모달 -> 오늘날짜가 start_date랑 end_date 사이인거만출력
    
    박람회 , 분기처리 고민
     환불 영수증 조회 기능 추가
    PENDING_PUBLISH일떄 결제 영수증 조회 기능
    광고 CANCELLED , PENDING_CANCEL 일떄 결제 정보
    
    광고/박람회 STATUS 별 환불 영수증 조회 기능 추가
    
    광고/박람회 환불 신청 영수증 좌우로 두탭으로 나누기
    
    예매 내역 최신순 조회로 변경, 두개로 나눠지는거 버그픽스
    
    박람회/광고 신청 내역에서 사진 꽉차게
    
    광고/박람회 신청현황 페이지네이션 바 << >> 삭제
    
    박람회 예매 상세내역 -> display date -> date 로 변경
    
    박람화 예매 상세내역 -> QR코드 박람회 게시기간 외 조회 불가능하게 수정
    
    실시간 혼잡도 표시 -> QR 코드 모달 안으로 이전
    
    실시간 혼잡도 , QR코드 EXPO  STATUS 분기처리
    예매 상세에서 포스터 클릭시 ->  박람회 상세 이동
    
    예매내역 / 결제내역 통합
    결제 내역 조회시에 마일리지 랑 결제 상세 정보  표시

--- src/main/java/com/myce/member/mapper/expo/ReservedExpoMapper.java
+++ src/main/java/com/myce/member/mapper/expo/ReservedExpoMapper.java
@@ -10,18 +10,24 @@ import java.util.stream.Collectors;
 @Component
 public class ReservedExpoMapper {
     
+    public ReservedExpoResponse toResponseDto(Reservation reservation) {
+        return ReservedExpoResponse.builder()
+                .expoId(reservation.getExpo().getId())
+                .title(reservation.getExpo().getTitle())
+                .thumbnailUrl(reservation.getExpo().getThumbnailUrl())
+                .ticketPrice(reservation.getTicket().getPrice())
+                .ticketCount(reservation.getQuantity())
+                .ticketName(reservation.getTicket().getName())
+                .reservationId(reservation.getId())
+                .reservationCode(reservation.getReservationCode())
+                .createdAt(reservation.getCreatedAt())
+                .reservationStatus(reservation.getStatus())
+                .build();
+    }
+    
     public List<ReservedExpoResponse> toResponseDtoList(List<Reservation> reservations) {
         return reservations.stream()
-                .map(reservation -> ReservedExpoResponse.builder()
-                        .expoId(reservation.getExpo().getId())
-                        .title(reservation.getExpo().getTitle())
-                        .thumbnailUrl(reservation.getExpo().getThumbnailUrl())
-                        .ticketPrice(reservation.getTicket().getPrice())
-                        .ticketCount(reservation.getQuantity())
-                        .ticketName(reservation.getTicket().getName())
-                        .reservationId(reservation.getId())
-                        .reservationCode(reservation.getReservationCode())
-                        .build())
+                .map(this::toResponseDto)
                 .collect(Collectors.toList());
     }
 }
