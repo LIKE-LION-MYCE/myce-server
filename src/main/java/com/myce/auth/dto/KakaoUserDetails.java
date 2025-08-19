9317746168c392ce526c91d606f0e41bcd9edb93
choeihwa <chldlghk@gmail.com>
2025-08-19 22:54:50 +0900

    fix: update kakao login scope

--- src/main/java/com/myce/auth/dto/KakaoUserDetails.java
+++ src/main/java/com/myce/auth/dto/KakaoUserDetails.java
@@ -11,30 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class KakaoUserDetails implements OAuth2UserInfo {
 
-    private final String name;
-    private final String email;
-    private final Gender gender;
-    private final LocalDate birth;
-    private final String phone;
     private Map<String, Object> attributes;
 
     public KakaoUserDetails(Map<String, Object> attributes) {
-        log.debug(String.valueOf(attributes.get("id")));
-
-        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-        this.email = (String) kakaoAccount.get("email");
-        this.name = String.valueOf(kakaoAccount.get("name"));
-        String genderStr = String.valueOf(kakaoAccount.get("gender")).toUpperCase();
-        this.gender = Gender.fromString(genderStr);
-        int birthYear = Integer.parseInt((String) kakaoAccount.get("birthyear"));
-        String birthDay = String.valueOf(kakaoAccount.get("birthday"));
-        int birthMonth = Integer.parseInt(birthDay.substring(0, 2));
-        int birthDayMonth = Integer.parseInt(birthDay.substring(2));
-        this.birth = LocalDate.of(birthYear, birthMonth,  birthDayMonth);
-        this.phone = formatPhoneNumber(String.valueOf(kakaoAccount.get("phone_number")));
-        log.debug(String.valueOf(kakaoAccount.get("phone_number")));
-        log.debug(phone);
-
         this.attributes = attributes;
     }
 
@@ -50,22 +29,13 @@ public class KakaoUserDetails implements OAuth2UserInfo {
 
     @Override
     public String getEmail() {
-        return this.email;
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        return String.valueOf(kakaoAccount.get("email"));
     }
 
     @Override
     public String getName() {
-        return name;
-    }
-
-    private static String formatPhoneNumber(String phoneNumber) {
-        if (phoneNumber == null || phoneNumber.isEmpty()) {
-            return null;
-        }
-
-        return phoneNumber
-                .replaceAll("\\+82\\s*", "0")  // +82 제거하고 0으로 시작
-                .replaceAll("\\s+", "")        // 공백 제거
-                .replaceAll("(\\d{3})(\\d{4})(\\d{4})", "$1-$2-$3"); // xxx-xxxx-xxxx 포맷
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        return String.valueOf(properties.get("nickname"));
     }
 }
